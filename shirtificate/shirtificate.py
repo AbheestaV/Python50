c08b00d836284aa92f7183d20b0aba1d92eeff21
Zod Mint <abheesta@gmail.com>
2023-05-08 11:31:56 -0700

    shirtificate finished

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,7 +1,21 @@
 from fpdf import FPDF
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.add_page()
-pdf.set_font("helvetica", "B", 16)
-pdf.cell(40, 10, "Hello World!")
-pdf.output("tuto1.pdf")
+class PDF(FPDF):
+    def header(self):
+        self.image("shirtificate.png", 10, 69, 180)
+        self.set_font("helvetica", "I", 69)
+        self.cell(0, 69, "CS50 Shirtificate", align="C")
+        self.ln(2)
+
+def pdfer(name):
+    pdf = PDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font("helvetica", "B", 16)
+    pdf.cell(40, 10, f"{name} took CS50", align="C")
+    pdf.output("shirtificate.pdf")
+
+def main():
+    pdfer(input("Name: "))
+
+if __name__ =="__main__":
+    main()
